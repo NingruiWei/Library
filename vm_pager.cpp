ee28fa35806f0b8f942908a1bdb4a1177f5d39ab
root <root@Ningruis-PC.localdomain>
2020-10-26 21:54:55 -0400

    stuff

--- vm_pager.cpp
+++ vm_pager.cpp
@@ -1,23 +1,43 @@
 #include "vm_pager.h"
+#include <vector>
 using namespace std;
 
+unsigned int physmem_size;
+unsigned int swap_size;
+struct pager_page_t{
+    page_table_entry_t base;
+    bool reference_bit = 0;
+};
+struct arena{
 
-void vm_init(unsigned int memory_pages, unsigned int swap_blocks){
+    page_table_t page_table;
+    pid_t process_id;
+    pid_t parent_id;
+};
+vector<arena> arena_table;
 
-
-}
+void vm_init(unsigned int memory_pages, unsigned int swap_blocks){  
+    physmem_size = memory_pages;
+    swap_size = swap_blocks;
+};
 
 int vm_create(pid_t parent_pid, pid_t child_pid){
+    if(parent_pid){
 
-
-
-}
+    }
+    else{
+    arena temp_arena;
+    temp_arena.parent_id = parent_pid; // null or w/e
+    temp_arena.process_id = child_pid;
+    arena_table.push_back(temp_arena);
+    }
+};
 
 void vm_switch(pid_t pid){
 
 
 
-}
+};
 
 
 
@@ -25,14 +45,14 @@ int vm_fault(const void* addr, bool write_flag){
 
 
 
-}
+};
 
 
 void vm_destroy(){
 
 
 
-}
+};
 
 void *vm_map(const char *filename, unsigned int block){
 
