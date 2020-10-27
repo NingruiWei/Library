5c86e865530b7cd1902c230c0dbea5ee162e9107
muytingc <muytingc@umich.edu>
2020-10-27 13:52:20 -0400

    went to office hours, understand more of infastructure, worked on vm_map

--- vm_app.cpp
+++ vm_app.cpp
@@ -1,15 +1,3 @@
 #include "vm_app.h"
 
 using namespace std;
-void *vm_map(const char *filename, unsigned int block){
-
-
-
-}
-
-
-void vm_yield(void){
-
-
-    
-}
--- vm_pager.cpp
+++ vm_pager.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 unsigned int physmem_size;
 unsigned int swap_size;
+
 struct pager_page_t{
     page_table_entry_t base;
     bool reference_bit = 0;
 };
+
 struct arena{
 
     page_table_t page_table;
     pid_t process_id;
-    pid_t parent_id;
+    uintptr_t arena_start = uintptr_t(VM_ARENA_BASEADDR);
+    uintptr_t arena_valid_end = uintptr_t(VM_ARENA_BASEADDR);
 };
+
 vector<arena> arena_table;
+int curr_arena_index = -1;
 
 void vm_init(unsigned int memory_pages, unsigned int swap_blocks){  
     physmem_size = memory_pages;
@@ -22,21 +27,23 @@ void vm_init(unsigned int memory_pages, unsigned int swap_blocks){
 };
 
 int vm_create(pid_t parent_pid, pid_t child_pid){
-    if(parent_pid){
+    if(parent_pid != 0){
 
     }
     else{
-    arena temp_arena;
-    temp_arena.parent_id = parent_pid; // null or w/e
-    temp_arena.process_id = child_pid;
-    arena_table.push_back(temp_arena);
+        arena temp_arena;
+        temp_arena.process_id = child_pid;
+        arena_table.push_back(temp_arena);
+        if(curr_arena_index == -1){
+            curr_arena_index = int(arena_table.size()) - 1;
+        }
     }
 };
 
 void vm_switch(pid_t pid){
-
-
-
+    //search arena table for arena with matching pid
+    //evict and writeback any necessary information from the page table leaving
+    //switch page_table_base_register to be a pointer to the new page_table_t from the new arena
 };
 
 
@@ -55,9 +62,28 @@ void vm_destroy(){
 };
 
 void *vm_map(const char *filename, unsigned int block){
+    if(arena_table[curr_arena_index].arena_valid_end == uintptr_t(VM_ARENA_BASEADDR) + VM_ARENA_SIZE){
+        //arena is full
+        return nullptr;
+    }
+    if(filename){
 
-
-    
+    }
+    else{
+        if(swap blocks not full){ //We need some way to track arena's use of swap blocks
+            int first_invalid_page = arena_valid_page_size() + 1;
+            page_table_entry_t temp_page;
+            temp_page.read_enable = 0;
+            temp_page.write_enable = 0;
+            page_table_base_register->ptes[first_invalid_page] = temp_page;
+            arena_table[curr_arena_index].arena_valid_end += VM_PAGESIZE;
+            return  (void *) arena_table[curr_arena_index].arena_valid_end;
+        }
+        else{
+            //Not enough swap blocks to hold all swap-backed virtual pages
+            return nullptr;
+        }
+    }
 }
 
 int file_read(const char* filename, unsigned int block, void* buf){
@@ -80,7 +106,7 @@ void* const vm_physmem{
 
 };
 
-
-page_table_t* page_table_base_register{
-
+int arena_valid_page_size(){
+    int arena_size = arena_table[curr_arena_index].arena_valid_end - arena_table[curr_arena_index].arena_start;
+    return arena_size / VM_PAGESIZE;
 }
