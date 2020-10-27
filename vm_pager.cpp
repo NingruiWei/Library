433bb02c0607bac009e89b1144188775f57c91db
root <root@Ningruis-PC.localdomain>
2020-10-26 20:40:50 -0400

    added rest of files in vm_pager

--- vm_pager.cpp
+++ vm_pager.cpp
@@ -1,5 +1,45 @@
 #include "vm_pager.h"
 using namespace std;
+
+
+void vm_init(unsigned int memory_pages, unsigned int swap_blocks){
+
+
+}
+
+int vm_create(pid_t parent_pid, pid_t child_pid){
+
+
+
+}
+
+void vm_switch(pid_t pid){
+
+
+
+}
+
+
+
+int vm_fault(const void* addr, bool write_flag){
+
+
+
+}
+
+
+void vm_destroy(){
+
+
+
+}
+
+void *vm_map(const char *filename, unsigned int block){
+
+
+    
+}
+
 int file_read(const char* filename, unsigned int block, void* buf){
 
 
