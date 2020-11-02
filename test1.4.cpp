c2750bb8b8f5f3019691f1c632444858717d8019
root <root@Ningruis-PC.localdomain>
2020-11-01 20:02:47 -0500

    hi

--- test1.4.cpp
+++ test1.4.cpp
@@ -13,13 +13,25 @@ int main()
     char *filename = (char *) vm_map(nullptr, 0);
 
     /* Write the name of the file that will be mapped */
+    cout << "hi lol adsf" << endl;
     strcpy(filename, "shakespeare.txt");
+    cout << "big pp" << endl;
 
     /* Map a page from the specified file */
     char *p = (char *) vm_map (filename, 0);
-
+    char* matt = (char*) vm_map (filename, 0);
     /* Print the first speech from the file */
     for (unsigned int i=0; i<2561; i++) {
 	    cout << p[i];
     }
+    for (unsigned int i=0; i<10; i++) {
+	    matt[i] = 'b';
+    }
+
+    for (unsigned int i=0; i<10; i++) {
+	    cout << matt[i];
+    }
+    
+
+
 }
