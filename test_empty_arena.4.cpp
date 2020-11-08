680bf934827c599589d727e8e29f6dbb41fb3b80
muytingc <muytingc@umich.edu>
2020-11-07 22:58:05 -0500

    found out that we have some segs with empty copying empty arenas (which is kind of silly since we were so focused on copying filled arenas). That's where we start tomorrow

---
+++ test_empty_arena.4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include "vm_app.h"
+using namespace std;
+int main()
+{
+    fork();
+    //fork();
+    
+    /* Allocate swap-backed page from the arena */
+    cout << "APP STARTED" << endl;
+    char *filename = (char *) vm_map(nullptr, 0);
+    cout << "after initial map with nullptr" << endl;
+    /* Write the name of the file that will be mapped */
+    strcpy(filename, "shakespeare.txt");
+    cout << "strcpy called" << endl;
+
+    /* Map a page from the specified file */
+    char *p = (char *) vm_map (filename, 0);
+
+    /* Print the first speech from the file */
+    for (unsigned int i=0; i<2561; i++) {
+	    cout << p[i];
+    }
+}
