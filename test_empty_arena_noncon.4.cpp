33772c2272c541772ad868c9ab9e4e20b7e4777e
muytingc <muytingc@umich.edu>
2020-11-08 17:42:27 -0500

    created new tests for empty arena forking (using other test cases) and added all new test cases to submit

---
+++ test_empty_arena_noncon.4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unistd.h>
+#include "vm_app.h"
+using namespace std;
+
+/*
+    Basically just test_noncontiguous_memory, but we empty arena fork at the beginning.
+*/
+
+int main()
+{
+    // tests filenames over two blocks
+    fork();
+
+    /* Allocate swap-backed page from the arena */
+    char *part_1 = (char *) vm_map(nullptr, 0);
+    char *part_2 = (char *) vm_map(nullptr, 0);
+
+    /* Write the name of the file that will be mapped */
+    // strcpy(filename1, ('*' * 4090));
+    part_1[4091] = 'd';
+    part_1[4092] = 'a';
+    part_1[4093] = 't';
+    part_1[4094] = 'a';
+    part_1[4095] = '4';
+    part_2[0] = '.';
+    part_2[1] = 'b';
+    part_2[2] = 'i';
+    part_2[3] = 'n';
+    part_2[4] = '\0';
+
+    cout << "Filename written" << endl;
+    char *p = (char *) vm_map (&part_1[4091], 0);
+    cout << "file mapped" << endl;
+    for (unsigned int i=0; i<100; i++) {
+	    cout << p[i];
+    }
+
+    cout << endl;
+}
---
+++ test_fork_empty_fork_nonempty.4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include "vm_app.h"
+
+using namespace std;
+
+/*
+    Basically just test 2, but we have an empty arena fork at the beginning
+*/
+int main()
+{
+    fork();
+
+    /* Allocate swap-backed page from the arena */
+    char *filename = (char *) vm_map(nullptr, 0);
+    cout << filename << endl;
+
+    /* Write the name of the file that will be mapped */
+    strcpy(filename, "data2.bin");
+    cout << filename << endl;
+
+    /* Map a page from the specified file */
+    char *paul = (char *) vm_map (filename, 0);
+
+    if (fork() == 0) {
+        cout << "FIRST CHILD" << endl;
+        strcpy(paul, "data1.bin");
+        for (unsigned int i=0; i<100; i++) {
+            cout << paul[i];
+        }
+        cout << endl;
+    }
+    else {
+        cout << "INSIDE ELSE" << endl;
+        if (fork() == 0) {
+            cout << "SECOND CHILD" << endl;
+            strcpy(paul, "data2.bin");
+            for (unsigned int i=0; i<100; i++) {
+                cout << paul[i];
+            }
+            cout << endl;
+        }
+        else {
+            cout << "SHAKE AND BAKE" << endl;
+            strcpy(paul, "shakespeare.txt");
+            for (unsigned int i=0; i<100; i++) {
+                cout << paul[i];
+            }
+            cout << endl;
+        }
+    }
+
+    cout << "end" << endl;  
+}
